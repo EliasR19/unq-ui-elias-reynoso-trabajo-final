22576deab1261332e80273393beecf92dff4f284
EliasR19 <ereynoso2040@gmail.com>
2026-07-13 16:55:07 -0300

    limpieza

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,5 +1,3 @@
-import { puntajesDePrueba } from "./utils";
-
 export const actualizarTop = (listaPuntajeNuevo) => {
     localStorage.setItem("top10", JSON.stringify(listaPuntajeNuevo));
 }
@@ -10,9 +8,3 @@ export const obtenerTop = () => {
 }
 
 
-export const inicializarPuntajesDePrueba = () => {
-    if (!localStorage.getItem("top10")) {
-        localStorage.setItem("top10", JSON.stringify(puntajesDePrueba));
-        console.log("¡Puntajes de prueba cargados con éxito!");
-    }
-};
